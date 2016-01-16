11136272c192947461d2f7b01bf3fe738f953067
zloioslik <evgeniy.v.zaytsev@gmail.com>
2016-01-16 15:07:29 +0300

    Initial commit

---
+++ app/src/main/java/ru/circle/literatorcircle/LoginActivity.java
@@ -0,0 +1,13 @@
+package ru.circle.literatorcircle;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
